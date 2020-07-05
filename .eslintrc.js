01bd83562b7445c73bcc329e4613ba5876cec37e
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-07-06 01:04:53 +0300

    SDS-3 add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,22 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "@typescript-eslint/no-var-requires": "off",
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
